4925cba3bc2ca47eb19c5b9485ceded9303afd49
avishkakavindud <avishkakavindud@gmail.com>
2022-11-28 18:08:42 +0530

    Validation pipelines

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth') // global prefix route
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(@Body() dto: AuthDto) {
+    console.log({ dto });
     return this.authService.signup();
   }
 
