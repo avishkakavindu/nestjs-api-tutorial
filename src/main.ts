85d773c41a60b8b01fedd214c6b430f3f4bfc329
avishkakavindud <avishkakavindud@gmail.com>
2022-12-06 16:29:19 +0530

    Validation on signup

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true, // strip out additional properties coming in req.body
+    }),
+  );
   await app.listen(4000);
 }
 bootstrap();
